c75b993305a47535c68345ef99bbafc443ea60a7
Carlos Rico <ricgarcar@gmail.com>
2023-07-28 13:11:30 +0200

    inicio ejercicio 8

--- 08_invertir_strings.js
+++ 08_invertir_strings.js
@@ -1,2 +1,10 @@
 // Crea un programa que invierta el orden de una cadena de texto sin usar funciones propias del lenguaje que lo hagan de forma automática.
 // * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+//
+
+function revertString(txt){
+ const txt = []
+}
+
+revertString("Hola Mundo")
+revertString("Hello World")
