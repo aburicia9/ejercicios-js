1d3de6d383f8620e77b2d2c58e6977eecd451634
Carlos Rico <ricgarcar@gmail.com>
2023-07-28 13:06:15 +0200

    ejer 6 completado

--- 06_adivina_un_integrante.js
+++ 06_adivina_un_integrante.js
@@ -1,2 +1,23 @@
 // Haremos un juego, el ordenador elegirá un número entre el 1 y el 10.
 // Y te preguntará el número un máximo de 3 veces: si aciertas tu ganas, si después de 3 intentos no aciertas, pierdes.
+
+
+   let num = Math.floor(Math.random() * 10) + 1;
+
+let intentos = 1;
+let numero = prompt("Introduce un numero");
+let numeroEntero = parseInt(numero);
+
+while (intentos < 3 && numeroEntero !== num) {
+  intentos++;
+  numero = prompt("Introduce un numero");
+  numeroEntero = parseInt(numero);
+}
+
+if (numeroEntero === num) {
+  console.log("Conseguido");
+} else {
+  console.log("Has perdido");
+}
+
+console.log(num)
